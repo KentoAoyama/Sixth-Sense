5a855a54d9b6b8ea1acd911901d878b7988c49b2
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2023-02-20 23:23:45 +0900

    [update]敵の撃破に関する処理の追加

--- Assets/Scripts/Enemy/EnemyDeath.cs
+++ Assets/Scripts/Enemy/EnemyDeath.cs
@@ -5,5 +5,51 @@ using UnityEngine;
 [System.Serializable]
 public class EnemyDeath
 {
-    
+    [Header("“ª‚Ì“–‚½‚è”»’è")]
+    [SerializeField]
+    private Collider _head;
+
+
+    [Header("‰E‚Ì“ñ‚Ì˜r‚Ì“–‚½‚è”»’è")]
+    [SerializeField]
+    private Collider _rightArm;
+
+    [Header("‰E˜r‚Ì“–‚½‚è”»’è")]
+    [SerializeField]
+    private Collider _rightForeArm;
+
+    [Header("‰Eè‚Ì“–‚½‚è”»’è")]
+    [SerializeField]
+    private Collider _rightHand;
+
+
+    [Header("¶‚Ì“ñ‚Ì˜r‚Ì“–‚½‚è”»’è")]
+    [SerializeField]
+    private Collider _leftArm;
+
+    [Header("¶˜r‚Ì“–‚½‚è”»’è")]
+    [SerializeField]
+    private Collider _leftForeArm;
+
+    [Header("¶è‚Ì“–‚½‚è”»’è")]
+    [SerializeField]
+    private Collider _leftHand;
+
+
+    [Header("‰Eã‹r‚Ì“–‚½‚è”»’è")]
+    [SerializeField]
+    private Collider _rightUpLeg;
+
+    [Header("‰E‹r‚Ì“–‚½‚è”»’è")]
+    [SerializeField]
+    private Collider _rightLeg;
+
+
+    [Header("¶ã‹r‚Ì“–‚½‚è”»’è")]
+    [SerializeField]
+    private Collider _leftUpLeg;
+
+    [Header("¶‹r‚Ì“–‚½‚è”»’è")]
+    [SerializeField]
+    private Collider _leftLeg;
 }
